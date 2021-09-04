59939275518c6f3f9e3aec90a65094d7f40f6cb3
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2021-09-04 15:19:49 +0300

    feat: socket chat

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,44 +1,60 @@
 import React, { useState, useEffect } from 'react'
 import { socket } from 'socket'
-import { JOIN_ROOM, RECEIVE_MESSAGE, SENT_MESSAGE } from 'constants'
+import {
+  JOIN_ROOM,
+  RECEIVE_MESSAGE,
+  SENT_MESSAGE,
+  USER_DISCONNECTED,
+} from 'constants'
 
 const App = () => {
   const [newMessage, setNewMessage] = useState('')
   const [chatHistory, setChatHistory] = useState([])
 
+  const joinSocketRoom = () => {
+    socket.emit(JOIN_ROOM, { room: 'room_name', id: Math.random() })
+  }
+
   const handleIncomingMessage = (incomingMessage) => {
-    setChatHistory((prevState) => ({
-      ...prevState,
-      incomingMessage,
-    }))
+    setChatHistory((prevState) => [...prevState, incomingMessage])
+  }
+
+  const handleUserDisconnect = (userId) => {
+    handleIncomingMessage(`${userId} disconnected`)
   }
 
   const addSocketListeners = () => {
-    socket.emit(JOIN_ROOM, 'room_name')
     socket.on(RECEIVE_MESSAGE, handleIncomingMessage)
+    socket.on(USER_DISCONNECTED, handleUserDisconnect)
   }
 
   const removeSocketListeners = () => {
     socket.off(RECEIVE_MESSAGE, handleIncomingMessage)
+    socket.off(USER_DISCONNECTED, handleUserDisconnect)
   }
 
   useEffect(() => {
+    joinSocketRoom()
     addSocketListeners()
     return () => removeSocketListeners()
   }, [])
 
   const handleSentMessage = () => {
     socket.emit(SENT_MESSAGE, newMessage)
+    setNewMessage('')
   }
 
   return (
     <div>
       <div>
         {chatHistory.map((chatMessage) => (
-          <span>{chatMessage}</span>
+          <div>{chatMessage}</div>
         ))}
       </div>
-      <input value={newMessage} onChange={setNewMessage} />
+      <input
+        value={newMessage}
+        onChange={(event) => setNewMessage(event.target.value)}
+      />
       <button type="button" onClick={handleSentMessage}>
         Sent
       </button>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
 export const JOIN_ROOM = 'join_room'
 export const RECEIVE_MESSAGE = 'receive_message'
 export const SENT_MESSAGE = 'sent_message'
+export const USER_DISCONNECTED = 'user_disconnected'
